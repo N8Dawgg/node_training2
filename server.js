5227a48ab0dbf5f2effd8eb7f9cbc7119e7d80ca
N8Dawgg <natwheeler1337@gmail.com>
2023-10-28 15:49:48 -0400

    Finishing up for today. Reorganized to prepare for MVC systems.

--- server.js
+++ server.js
@@ -30,11 +30,9 @@ app.use(express.json());
 
 //server static files
 app.use("/", express.static(path.join(__dirname, "/public")));
-app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
 //routes
 app.use("/", require("./routes/root.js"));
-app.use("/subdir", require("./routes/subdir.js"));
 app.use("/employees", require("./routes/api/employees.js"));
 
 app.all("*", (req, res) => {
